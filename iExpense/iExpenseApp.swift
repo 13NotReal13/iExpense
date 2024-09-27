53e687afa4e9d241f0efa3b94c32dec85a9934c4
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-27 20:01:20 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Иван Семикин on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
